511b757df401821cd3204a743c99219609e30b4b
Luiz <luizmitsuru@gmail.com>
2020-05-26 21:31:40 -0300

    Primeira atualização lista e botões adicionados

--- src/App.js
+++ src/App.js
@@ -5,20 +5,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>Quantidade de Produtos:</header>
+      <lu>
+        <li>item1</li>
+        <button>Adicionar ao carrinho</button>
+        <li>item2</li>
+        <button>Adicionar ao carrinho</button>
+        <li>item3</li>
+        <button>Adicionar ao carrinho</button>
+        <li>item4</li>
+        <button>Adicionar ao carrinho</button>
+        <li>item5</li>
+        <button>Adicionar ao carrinho</button>
+        <li>item6</li>
+        <button>Adicionar ao carrinho</button>
+        <li>item7</li>
+        <button>Adicionar ao carrinho</button>
+        <li>item8</li>
+        <button>Adicionar ao carrinho</button>
+      </lu>
     </div>
   );
 }
